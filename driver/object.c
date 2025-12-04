032037cbb15acfca021e8f484092cf3d52c6cccf
Michael Saviour <106999975+saviour623@users.noreply.github.com>
2025-12-04 14:48:37 +0000

    object

--- driver/object.c
+++ driver/object.c
@@ -1,10 +1,23 @@
 #include <stdint.h>
 #include <immintrin.h>
 
-struct object {
+#define OBJDEF_SIZE ((16 * 2) + 16)
+
+typedef struct __object __object;
+typedef __object * tobject;
+
+struct __object
+{
     uint64_t __meta[2];
-    void *__dat;
-    void *__cache;
-    void *__next;
+    void    *__dat;
+    void    *__cache;
+    struct   __object *__next;
     uint16_t __size;
-};
+}
+
+ static struct __object * __attribute__((noinline, warn_unused)) __initobject(uint32_t size __attribute_maybe_unused__)
+{
+    struct __object *object;
+
+    if ((object = malloc(sizeof(struct __object) + OBJDEF_SIZE))) return NULL;
+}
